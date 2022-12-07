805ba3ab98882e8cc0e43d8a0ef81f0939daaa16
Thibaut Vandervelden <thvdveld@vub.be>
2022-12-07 10:27:26 +0200

    add url to wolfcrypt

--- examples/wolfcrypt/wolfcrypt.c
+++ examples/wolfcrypt/wolfcrypt.c
@@ -2,7 +2,10 @@
 
 #include "wolfssl/wolfcrypt/types.h"
 
+// Information can be found here:
+// https://www.wolfssl.com/documentation/manuals/wolfssl/group__SHA.html
 // TODO: INCLUDE THE CORRECT HEADERS HERE:
+
 
 #include <stdio.h> /* For printf() */
 /*---------------------------------------------------------------------------*/
@@ -10,21 +13,21 @@ PROCESS(wolfcrypt_process, "Wolfcrypt process");
 AUTOSTART_PROCESSES(&wolfcrypt_process);
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(wolfcrypt_process, ev, data) {
-  static struct etimer timer;
+    static struct etimer timer;
 
-  PROCESS_BEGIN();
+    PROCESS_BEGIN();
 
-  /* Setup a periodic timer that expires after 10 seconds. */
-  etimer_set(&timer, CLOCK_SECOND);
+    /* Setup a periodic timer that expires after 10 seconds. */
+    etimer_set(&timer, CLOCK_SECOND);
 
-  while (1) {
-    // ADD WOLFCRYPT METHODS HERE:
+    while (1) {
+        // ADD WOLFCRYPT METHODS HERE:
 
-    /* Wait for the periodic timer to expire and then restart the timer. */
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-    etimer_reset(&timer);
-  }
+        /* Wait for the periodic timer to expire and then restart the timer. */
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+        etimer_reset(&timer);
+    }
 
-  PROCESS_END();
+    PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
